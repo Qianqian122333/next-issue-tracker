e0fb6eccc8bfbd5aa4e66b24c8e0a1b914295e35
Qianqian Wei <qianqianwei112233@gmail.com>
2025-11-03 17:14:11 +0800

    Customize Radix UI theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import "@radix-ui/themes/styles.css";
-import { Theme } from "@radix-ui/themes";
+import { Theme, ThemePanel } from "@radix-ui/themes";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -30,7 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Theme>
+        <Theme accentColor="violet">
           <NavBar />
           <main className="p-5">{children}</main>
         </Theme>
